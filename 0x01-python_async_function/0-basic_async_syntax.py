6d101337b4ddd6b83e9268160d69e4badf0dd8bd
TzStrikerYT <maicol.asus@gmail.com>
2020-10-20 12:54:46 -0500

    [fix] pep8 style

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,9 +4,9 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """ asyncronous program that return a float number 
-        with a random wait delay 
+async def wait_random(max_delay: int=10) -> float:
+    """asyncronous program that return a float number
+        with a random wait delay
     """
 
     randomVal = random.uniform(0, max_delay)
