520a88950e3fe395a7fded06e04020e1ff8ca056
TzStrikerYT <maicol.asus@gmail.com>
2020-10-20 12:51:45 -0500

    [add] task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" My first async program """
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ asyncronous program that return a float number 
+        with a random wait delay 
+    """
+
+    randomVal = random.uniform(0, max_delay)
+    await asyncio.sleep(randomVal)
+    return randomVal
