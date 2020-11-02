6b0823ae90d3e4afd11f85f4d770a6c75d6edbec
TzStrikerYT <maicol.asus@gmail.com>
2020-11-02 04:21:23 -0500

    [add] add task 0

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Personal data project """
+from typing import List
+import re
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ returns the log message obfuscated """
+    for i in fields:
+        message = re.sub(fr'{i}=.+?{separator}',
+                         f'{i}={redaction}{separator}', message)
+    return message
