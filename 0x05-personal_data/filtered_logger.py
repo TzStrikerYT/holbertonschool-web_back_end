54c9e87c2bb12de3fe9c07192306553bbc883fad
TzStrikerYT <maicol.asus@gmail.com>
2020-11-02 04:22:35 -0500

    [fix] fix pep8 style

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 from typing import List
 import re
 
+
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ returns the log message obfuscated """
     for i in fields:
