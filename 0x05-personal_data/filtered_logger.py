d8180fe31e527c0cb60488f7c9acbf995589d5b3
TzStrikerYT <maicol.asus@gmail.com>
2020-11-02 04:32:42 -0500

    [fix] fix arguments of the function

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,7 +4,10 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """ returns the log message obfuscated """
     for i in fields:
         message = re.sub(fr'{i}=.+?{separator}',
