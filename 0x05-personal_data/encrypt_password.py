94dcad7432f22f7f33036949ac0866400c89a3e7
TzStrikerYT <maicol.asus@gmail.com>
2020-11-02 22:04:32 -0500

    [add] task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hash encryption """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if pass is valid """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
