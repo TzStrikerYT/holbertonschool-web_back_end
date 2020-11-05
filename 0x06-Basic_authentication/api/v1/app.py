411ebeeab56fd43f10e733441fbe1ace8cef4a31
TzStrikerYT <maicol.asus@gmail.com>
2020-11-05 16:53:09 -0500

    [add] add task 1

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -3,11 +3,10 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from flask_cors import (CORS)
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,6 +18,13 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def not_found(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
