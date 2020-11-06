068469707bb25d951d7424f235dfe4d877bad2aa
TzStrikerYT <maicol.asus@gmail.com>
2020-11-06 13:37:06 -0500

    [add] task 9

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,20 @@ class BasicAuth(Auth):
             return decode
         except (AttributeError, ValueError) as a:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """ returns the user email and password
+        from the Base64 decoded value """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        return tuple(decoded_base64_authorization_header.split(":", 1))
