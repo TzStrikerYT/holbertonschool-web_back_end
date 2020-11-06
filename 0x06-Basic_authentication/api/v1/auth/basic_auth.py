fd2bc1d95b0bc606c8059fc2705188d92693f4cd
TzStrikerYT <maicol.asus@gmail.com>
2020-11-06 13:11:18 -0500

    [add] ass task 7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic auth class """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header for a Basic Authentication """
+        if len(str(authorization_header).split(" ")) == 2:
+            if authorization_header \
+                    is not None and 'Basic' in str(authorization_header).split(" ")[0]:
+                return authorization_header.split(" ")[1]
+        
+        return None
