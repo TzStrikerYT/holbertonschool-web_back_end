b55316b1c507de4f65c4721f95dac57908ca128c
TzStrikerYT <maicol.asus@gmail.com>
2020-11-16 22:24:21 -0500

    [add] add a register function with authentication

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 """ auth class """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
     """ Transform to Hash format """
     return bcrypt.hashpw(password=password.encode(), salt=bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register a new user with authentication """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            pwd_encrypt = _hash_password(password=password)
+            return self._db.add_user(email=email, hashed_password=pwd_encrypt)
+        else:
+            raise ValueError("User {} already exists".format(email))
