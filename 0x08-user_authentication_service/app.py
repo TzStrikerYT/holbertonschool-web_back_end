c0588095360fb763010e0a0151ffda1ec7a861dd
TzStrikerYT <maicol.asus@gmail.com>
2020-11-18 13:44:53 -0500

    [add] add a get methon for user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ APP """
-from flask import jsonify, Flask
+from flask import jsonify, Flask, request, abort
 from auth import Auth
 
 
@@ -12,6 +12,26 @@ AUTH = Auth()
 def main():
     """ Main execute """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def get_user():
+    """ Get an user """
+    email = request.form.get('email')
+    passwd = request.form.get('password')
+
+    if email is None or passwd is None:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email=email, password=passwd)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({
+            "email": f"{user.email}",
+            "message": "user created"
+        }), 200
 
 
 if __name__ == "__main__":
