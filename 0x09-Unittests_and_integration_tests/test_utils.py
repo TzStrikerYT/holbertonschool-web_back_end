b84af37fa8111ea710257be8e2915a8f087b7668
TzStrikerYT <maicol.asus@gmail.com>
2020-11-24 19:04:05 -0500

    [add] add error test

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_value):
         """ unittest for the function """
         self.assertEqual(access_nested_map(nested_map, path), expected_value)
+
+    @parameterized.expand([
+        ({}, ('a',), KeyError('a')),
+        ({"a": 1}, ("a", "b"), KeyError('b'))
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_value):
+        """ Unittest for exception in a nested map function """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map=nested_map, path=path)
+
+        self.assertEqual(repr(error.exception), repr(expected_value))
