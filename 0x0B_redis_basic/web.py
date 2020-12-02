54c57766a61b796ce870e727981ec077ac6d9e7b
TzStrikerYT <maicol.asus@gmail.com>
2020-12-01 23:45:38 -0500

    [test]

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,5 +1,5 @@
-# !/usr/bin/env python3
-""" Redis with requests """
+#!/usr/bin/env python3
+""" Module to practice the previous concepts in Redis with requests """
 import requests
 from functools import wraps
 from typing import Callable
@@ -7,16 +7,17 @@ import redis
 
 
 def count(method: Callable):
-    """ Count the call to requests """
+    """Count the call to requests"""
     r = redis.Redis()
 
     @wraps(method)
     def wrapped(url):
-        """ function that will count """
+        """EXtra behavior to function that will count"""
         r.incr(f"count:{url}")
         expiration_count = r.get(f"cached:{url}")
         if expiration_count:
             return expiration_count.decode('utf-8')
+
         html = method(url)
         r.setex(f"cached:{url}", 10, html)
         return html
@@ -26,5 +27,7 @@ def count(method: Callable):
 
 @count
 def get_page(url: str) -> str:
-    """ module to obtain the HTML """
+    """It uses the requests module to obtain the
+    HTML content of a particular URL and returns it.
+    """
     return requests.get(url).text
