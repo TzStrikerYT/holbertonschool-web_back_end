c7601502939db9b9ad771fad5d8f5bbec7c6734a
TzStrikerYT <maicol.asus@gmail.com>
2020-12-01 23:40:54 -0500

    [add] add advanced task

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python3
+""" Redis with requests """
+import requests
+from functools import wraps
+from typing import Callable
+import redis
+
+
+def count(method: Callable):
+    """ Count the call to requests """
+    r = redis.Redis()
+
+    @wraps(method)
+    def wrapped(url):
+        """ function that will count """
+        r.incr(f"count:{url}")
+        expiration_count = r.get(f"cached:{url}")
+        if expiration_count:
+            return expiration_count.decode('utf-8')
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapped
+
+
+@count
+def get_page(url: str) -> str:
+    """ module to obtain the HTML """
+    return requests.get(url).text
