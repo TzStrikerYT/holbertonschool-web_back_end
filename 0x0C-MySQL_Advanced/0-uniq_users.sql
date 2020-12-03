a681cc6197e86f5b7b4be97a60931eee14c6573b
TzStrikerYT <maicol.asus@gmail.com>
2020-12-03 12:06:03 -0500

    [fix] INT var in the table

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- MYSQL advanced project
 -- script that creates a users table
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL
 );
