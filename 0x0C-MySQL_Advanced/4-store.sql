0409ea08985202d316e4989747ecb9d158c52d92
TzStrikerYT <maicol.asus@gmail.com>
2020-12-04 18:11:46 -0500

    [fix] syntax

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,6 +1,5 @@
  --creates a trigger that decreases the quantity of an item after adding a new order
-CREATE TRIGGER decrase_quant after insert
+CREATE trigger decrase_quant after insert
 ON orders for each row update items
-
 SET quantity = quantity - NEW.number
 WHERE NEW.item_name 
