b070d25f994af3c5798b9e1cece09b21819cd7e6
TzStrikerYT <maicol.asus@gmail.com>
2020-12-04 22:46:59 -0500

    [add] add task 5

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,11 @@
- -- creates a trigger that resets the attribute valid_email only WHEN the email has been changed
+-- creates a trigger that resets the attribute valid_email only WHEN the email has been changed
 delimiter //
-CREATE trigger reset_valid before update
-ON users for each row BEGIN if NEW.email <> OLD.email THEN
-
-SET NEW.valid_email = 0; end if; END // 
+CREATE trigger reset_valid 
+before update ON users
+for each row 
+BEGIN 
+    if NEW.email <> OLD.email 
+    THEN SET NEW.valid_email = 0;
+    end if;
+END // 
 delimiter ;
