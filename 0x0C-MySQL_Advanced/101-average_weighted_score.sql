de8de130f5ec979c052d0ef2e75718e3a9803d65
TzStrikerYT <maicol.asus@gmail.com>
2020-12-04 23:18:01 -0500

    [add] add task 13

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,17 @@
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers() BEGIN UPDATE users AS U, (
+SELECT  U.id
+       ,SUM(score * weight) / SUM(weight) AS w_avg
+FROM users AS U
+JOIN corrections AS C
+ON U.id=C.user_id
+JOIN projects AS P
+ON C.project_id=P.id
+GROUP BY  U.id) AS WA
+
+SET U.average_score = WA.w_avg
+WHERE U.id=WA.id;
+END // 
+DELIMITER ; 
