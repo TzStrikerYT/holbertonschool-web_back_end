f06fdef308d5abd78ada0ae2b1127ea96ecb2116
TzStrikerYT <maicol.asus@gmail.com>
2020-12-04 23:19:32 -0500

    [add] add task 13

--- 0x0C-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,3 +1,5 @@
+-- Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers
+-- that computes and store the average weighted score for all students.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 
 DELIMITER //
