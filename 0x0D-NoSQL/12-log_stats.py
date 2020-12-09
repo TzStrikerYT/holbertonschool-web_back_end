85a44177f9dec3442eff4702f8f20ec6e4b67ee3
TzStrikerYT <maicol.asus@gmail.com>
2020-12-08 22:27:56 -0500

    [add] add function that display stats of nginx logs

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Print info f nginx logs """
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ check for all elements in a collection """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(f"{collection.estimated_document_count()} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
+
+    check_get = collection.count_documents({'method': 'GET', 'path': "/status"})
+    print(f"{check_get} status check")
