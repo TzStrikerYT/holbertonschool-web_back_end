a949bd5623632776635b6998765897cb22335fb7
TzStrikerYT <maicol.asus@gmail.com>
2020-12-08 21:05:35 -0500

    [add] python function that list all dpcuments

---
+++ 0x0D-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import pymongo
+
+
+def list_all(mongo_collection):
+    """ List all elements in a collection """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
