10f17f46d45fa40ca955fa16febb5a631e213fed
TzStrikerYT <maicol.asus@gmail.com>
2020-12-16 14:59:39 -0500

    [fix] fix console log error

--- 0x0F-ES6_promise/7-load_balancer.js
+++ 0x0F-ES6_promise/7-load_balancer.js
@@ -1,5 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]).then((value) => {
-    console.log(value);
-  });
+  return Promise.race([chinaDownload, USDownload]);
 }
