eefe2849f97e5be79e16d5009ffaa265e24a873e
TzStrikerYT <maicol.asus@gmail.com>
2020-12-16 16:30:11 -0500

    [Holberton requirements are stupid]

--- 0x0F-ES6_promise/.eslintrc.js
+++ 0x0F-ES6_promise/.eslintrc.js
@@ -1,14 +1,33 @@
-export function uploadPhoto() {
-    return Promise.resolve({
-	status: 200,
-	body: 'photo-profile-1',
-    });
-}
-
-
-export function createUser() {
-    return Promise.resolve({
-	firstName: 'Guillaume',
-	lastName: 'Salva',
-    });
-}
+module.exports = {
+    env: {
+	browser: false,
+	es6: true,
+	jest: true,
+    },
+    extends: [
+	'airbnb-base',
+	'plugin:jest/all',
+    ],
+    globals: {
+	Atomics: 'readonly',
+	SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+	ecmaVersion: 2018,
+	sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+	'no-console': 'off',
+	'no-shadow': 'off',
+	'no-restricted-syntax': [
+	    'error',
+	    'LabeledStatement',
+	    'WithStatement',
+	],
+    },
+    overrides:[
+	{
+	    files: ['*.js'],
+	    excludedFiles: 'babel.config.js',
+	}
