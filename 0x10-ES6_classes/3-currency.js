4eb09f51f51326282da94feda8ea2510327168de
TzStrikerYT <maicol.asus@gmail.com>
2020-12-21 18:34:51 -0500

    [add] add task 3, methods

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
