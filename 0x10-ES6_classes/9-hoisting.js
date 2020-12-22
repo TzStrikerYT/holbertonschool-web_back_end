4c88f156cbf24fd53cbe368c3a979b9f14cfe98b
TzStrikerYT <maicol.asus@gmail.com>
2020-12-21 23:12:41 -0500

    [fix] eslint Style, and new function

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -1,39 +1,39 @@
 export class HolbertonClass {
-    constructor(year, location) {
-	this._year = year;
-	this._location = location;
-    }
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
 
-    get year() {
-	return this._year;
-    }
+  get year() {
+    return this._year;
+  }
 
-    get location() {
-	return this._location;
-    }
+  get location() {
+    return this._location;
+  }
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-    constructor(firstName, lastName, holbertonClass) {
-	this._firstName = firstName;
-	this._lastName = lastName;
-	this._holbertonClass = holbertonClass;
-    }
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
 
-    get fullName() {
-	return `${this._firstName} ${this._lastName}`;
-    }
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
 
-    get holbertonClass() {
-	return this._holbertonClass;
-    }
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
 
-    get fullStudentDescription() {
-	return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-    }
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
 }
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
