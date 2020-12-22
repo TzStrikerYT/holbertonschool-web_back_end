9f226ee674d5eff942caf0992f77698c225cdf58
TzStrikerYT <maicol.asus@gmail.com>
2020-12-21 20:44:19 -0500

    [fix] fix task 5, typeof !== 'function' for evacuationWarningMessage

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
