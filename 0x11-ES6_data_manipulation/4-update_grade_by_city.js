ee6042ed463ef5e8b60e37d58934323e3032ef05
TzStrikerYT <maicol.asus@gmail.com>
2020-12-23 19:32:22 -0500

    [fix] Fix eslint and var of object that stay bad :v

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
-export default function updateStudentGradeByCity(studentsArray, city, newGrades) {
-    return studentsArray.filter(item => item.location === city).map(student => {
-	const grades = newGrades.filter(item => item.studentId === student.id).map(pos => pos.grades)[0];
-	const grade = grades || 'N/A';
-	return { ...student, grade };
-    });
+export default function updateStudentGradeByCity(studentsArray, city, newGds) {
+  return studentsArray.filter((item) => item.location === city).map((student) => {
+    const gitem = newGds.filter((item) => item.studentId === student.id).map((pos) => pos.grade)[0];
+    const grade = gitem || 'N/A';
+    return { ...student, grade };
+  });
 }
