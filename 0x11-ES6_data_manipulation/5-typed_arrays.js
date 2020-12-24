5168765d72dedd1ae1b42a9d382fa6678fda394d
TzStrikerYT <maicol.asus@gmail.com>
2020-12-23 20:52:51 -0500

    [fix] fix DataView for display holberton example

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) throw Error('Position outside range');
   const buff = new ArrayBuffer(length);
-  const int8 = new Int8Array(buff);
-
-  int8[position] = value;
+  const int8 = new DataView(buff, 0);
+  int8.setInt8(position, value);
   return int8;
 }
