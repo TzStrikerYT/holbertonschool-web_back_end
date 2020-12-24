5dfaa6ccbc33bc8efda70231dcc11febc179ac69
TzStrikerYT <maicol.asus@gmail.com>
2020-12-23 20:40:50 -0500

    [fix] fix eslint style, 1 error remaining

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position >= length) throw Error('Position outside range');
-    const buff = new ArrayBuffer(length);
-    const int8 = new Int8Array(buff);
+  if (position >= length) throw Error('Position outside range');
+  const buff = new ArrayBuffer(length);
+  const int8 = new Int8Array(buff);
 
-    int8[position] = value;
-    return int8;
+  int8[position] = value;
+  return int8;
 }
