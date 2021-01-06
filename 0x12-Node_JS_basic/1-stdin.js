7d7b887db36e9a53f444b98314b5e6b3a25acc43
TzStrikerYT <maicol.asus@gmail.com>
2021-01-06 14:49:52 -0500

    [fix] Y for y line 4

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name) process.stdout.write(`your name is: ${name}`);
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
