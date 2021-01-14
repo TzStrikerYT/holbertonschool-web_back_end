3fafc9b5533637199e4a592a441a48b1c23eee5e
TzStrikerYT <maicol.asus@gmail.com>
2021-01-13 21:12:39 -0500

    [fix] divde the functions

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,14 +4,20 @@ const assert = require('assert');
 describe('calculateNumber', () => {
     it('checks ouput', () => {
         assert.strictEqual(calculateNumber(1.5, 2.1), 4);
-        assert.strictEqual(calculateNumber(0, 0.0), 0)
+    });
+    it('checks ouput', () => {
+        assert.strictEqual(calculateNumber(0, 0.0), 0);
     });
     it('Checks Negatives Numbers', ()=> {
+        assert.strictEqual(calculateNumber(-10, -5), -15);
+    })
+    it('Checks Negatives Numbers', ()=> {
         assert.strictEqual(calculateNumber(-2, -2), -4);
-        assert.strictEqual(calculateNumber(-10, -5), -15)
     })
     it('If can convert to number', ()=> {
         assert.strictEqual(isNaN(calculateNumber(5)), true);
+    })
+    it('If can convert to number', ()=> {
         assert.strictEqual(isNaN(calculateNumber('string')), true)
     })
 })
