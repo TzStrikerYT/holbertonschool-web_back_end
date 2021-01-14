8ac035140ef670df57473477b4fe6b0bee3db153
TzStrikerYT <maicol.asus@gmail.com>
2021-01-13 22:10:22 -0500

    [add] modify funtions

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,15 +1,17 @@
 const calculateNumber = (type, a, b) => {
+    if (isNaN(a) || isNaN(b))
+      throw new TypeError();
     switch (type) {
       case 'SUM':
         return Math.round(a) + Math.round(b);
-        break;
-      case 'SUBSTRACT':
+      case 'SUBTRACT':
         return Math.round(a) - Math.round(b);
-        break;
       case 'DIVIDE':
         if (Math.round(b) === 0) return 'Error';
         return Math.round(a) / Math.round(b);
-        break;
-    };
-  }
+      default:
+        throw new TypeError;
+    }
+  };
+  
   module.exports = calculateNumber;
