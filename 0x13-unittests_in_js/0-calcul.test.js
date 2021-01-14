2f9ce36101dffe07b294762d65688f0ca6a085d3
TzStrikerYT <maicol.asus@gmail.com>
2021-01-13 21:45:59 -0500

    [add] test restructure tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,38 +1,17 @@
 const calculateNumber = require('./0-calcul')
 const assert = require('assert');
 
-describe('calculateNumber step 1', () => {
-    it('checks ouput', () => {
-        assert.strictEqual(calculateNumber(1.5, 2.1), 4);
-    });
-})
-
-describe('calculateNumber step 2', () => {
-    it('checks ouput part2', () => {
-        assert.strictEqual(calculateNumber(0, 0.0), 0);
-    });
-})
-
-describe('calculateNumber step 2 negatives', () => {
-    it('Checks Negatives Numbers', ()=> {
-        assert.strictEqual(calculateNumber(-10, -5), -15);
-    });
-})
-
-describe('calculateNumber step 3 negatives v2', () => {
-    it('Checks Negatives Numbers part2', ()=> {
-        assert.strictEqual(calculateNumber(-2, -2), -4);
-    });
-})
-
-describe('Coverts to number', () => {
-    it('If can convert to number', ()=> {
-        assert.strictEqual(isNaN(calculateNumber(5)), true);
-    });
-})
-
-describe('Coverts to number part2', () => {
-    it('If can convert to number part2', ()=> {
-        assert.strictEqual(isNaN(calculateNumber('string')), true)
-    });
-})
+describe('calculateNumber', function () {
+      it('should return 4', function () {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(3.7, 1), 5);
+        assert.strictEqual(calculateNumber(1, 3.3), 4);
+        assert.strictEqual(calculateNumber(3.3, 1), 4);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(3.7, 1.2), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+        assert.strictEqual(calculateNumber(3.7, 1.2), 5);
+        assert.strictEqual(calculateNumber(1.2, 2.1), 3);
+      });
+  })
