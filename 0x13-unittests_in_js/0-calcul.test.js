391eccdefb6efd2eeab164fc604ca1162fb0d7b7
TzStrikerYT <maicol.asus@gmail.com>
2021-01-13 20:52:02 -0500

    [add] add negative numbers

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,9 +2,13 @@ const calculateNumber = require('./0-calcul')
 const assert = require('assert');
 
 describe('calculateNumber', () => {
-    it('checks outut', () => {
+    it('checks ouput', () => {
         assert.equal(calculateNumber(1.5, 2.1), 4);
         assert.equal(calculateNumber(0, 0.0), 0)
+    });
+    it('Checks Negatives Numbers', ()=> {
+        assert.equal(calculateNumber(-2, -2), -4);
+        assert.equal(calculateNumber(-10, -5), -15)
     })
 })
 
