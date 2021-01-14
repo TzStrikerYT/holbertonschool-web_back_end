7a207cd490f2221e519794bb0d3c4ef3487e8693
TzStrikerYT <maicol.asus@gmail.com>
2021-01-13 21:30:55 -0500

    [add] add absurd tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,38 @@
 const calculateNumber = require('./0-calcul')
 const assert = require('assert');
 
-describe('calculateNumber', () => {
+describe('calculateNumber step 1', () => {
     it('checks ouput', () => {
         assert.strictEqual(calculateNumber(1.5, 2.1), 4);
     });
-    it('checks ouput', () => {
-        assert.strictEqual(calculateNumber(0, 0.0), 0);
-    });
-    it('Checks Negatives Numbers', ()=> {
-        assert.strictEqual(calculateNumber(-10, -5), -15);
-    })
-    it('Checks Negatives Numbers', ()=> {
-        assert.strictEqual(calculateNumber(-2, -2), -4);
-    })
-    it('If can convert to number', ()=> {
-        assert.strictEqual(isNaN(calculateNumber(5)), true);
-    })
-    it('If can convert to number', ()=> {
-        assert.strictEqual(isNaN(calculateNumber('string')), true)
-    })
 })
 
+describe('calculateNumber step 2', () => {
+    it('checks ouput part2', () => {
+        assert.strictEqual(calculateNumber(0, 0.0), 0);
+    });
+})
+
+describe('calculateNumber step 2 negatives', () => {
+    it('Checks Negatives Numbers', ()=> {
+        assert.strictEqual(calculateNumber(-10, -5), -15);
+    });
+})
+
+describe('calculateNumber step 3 negatives v2', () => {
+    it('Checks Negatives Numbers part2', ()=> {
+        assert.strictEqual(calculateNumber(-2, -2), -4);
+    });
+})
+
+describe('Coverts to number', () => {
+    it('If can convert to number', ()=> {
+        assert.strictEqual(isNaN(calculateNumber(5)), true);
+    });
+})
+
+describe('Coverts to number part2', () => {
+    it('If can convert to number part2', ()=> {
+        assert.strictEqual(isNaN(calculateNumber('string')), true)
+    });
+})
