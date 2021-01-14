a57eddfb1bea625cb14847ba6fcf796c4bc9c4f5
TzStrikerYT <maicol.asus@gmail.com>
2021-01-13 21:47:23 -0500

    [add] test restructure tests

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const calculateNumber = require('./0-calcul')
 const assert = require('assert');
 
 describe('calculateNumber', function () {
-      it('should return 4', function () {
+      it('test function with values', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
         assert.strictEqual(calculateNumber(3.7, 1), 5);
