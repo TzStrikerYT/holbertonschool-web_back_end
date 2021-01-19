3e607d2d58c21527cd1f756b7085f55a8e609e39
TzSTrikerYT <maicol.asus@gmail.com>
2021-01-19 16:22:48 -0500

    [add] create and print the key value and set new key in the server

---
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,22 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.once('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (err, reply) => console.log(reply) );
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
