e9c32ed8596555cc4b1a4147243ee24a5573d37b
TzSTrikerYT <maicol.asus@gmail.com>
2021-01-19 16:56:35 -0500

    [add] add function that creates diferents keys at the same time

---
+++ 0x14-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,21 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.once('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+});
+
+const keys = ['Portland', 'Seattle', 'New York', 'Bogota', 'Cali', 'Paris'];
+const values = [50, 80, 20, 20, 40, 2];
+
+keys.forEach((key, i) => {
+    client.hset('HolbertonSchools', key, values[i], redis.print);
+});
+
+client.hgetall('HolbertonSchools', (error, value) => {
+    console.log(value)
+})
