e2330cf3ec49098241f1ef013f6a316eff646457
TzSTrikerYT <maicol.asus@gmail.com>
2021-01-19 16:09:53 -0500

    [add] task 0, verify connection with redis

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.once('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+});
